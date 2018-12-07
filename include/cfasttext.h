1449d3ad44659e35d2fd3549aacf509c8da334c7
Viktor Taranenko <viktortnk@gmail.com>
2018-12-07 22:43:28 +0000

    fix missing semicolon

--- include/cfasttext.h
+++ include/cfasttext.h
@@ -124,7 +124,7 @@ CFASTTEXT_API bool cft_fasttext_is_quant(fasttext_t* handle);
 CFASTTEXT_API void cft_fasttext_load_vectors(fasttext_t* handle, const char* filename, char** errptr);
 CFASTTEXT_API void cft_fasttext_train(fasttext_t* handle, fasttext_args_t* args, char** errptr);
 CFASTTEXT_API fasttext_predictions_t* cft_fasttext_predict(fasttext_t* handle, const char* text, int32_t k, float threshold);
-CFASTTEXT_API fasttext_predictions_t* cft_fasttext_predict_on_words(fasttext_t* handle, fasttext_words_t* words, int32_t k, float threshold)
+CFASTTEXT_API fasttext_predictions_t* cft_fasttext_predict_on_words(fasttext_t* handle, fasttext_words_t* words, int32_t k, float threshold);
 CFASTTEXT_API void cft_fasttext_predictions_free(fasttext_predictions_t* predictions);
 CFASTTEXT_API void cft_fasttext_quantize(fasttext_t* handle, fasttext_args_t* args, char** errptr);
 CFASTTEXT_API fasttext_tokens_t* cft_fasttext_tokenize(fasttext_t* handle, const char* text);
